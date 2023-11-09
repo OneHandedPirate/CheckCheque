c50e95746cdf368c4b5b58aabe6e08113fc28f67
onehandedpirate <plagueismkii@gmail.com>
2023-11-09 17:16:20 +1000

    add state for custom date input

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,8 @@ import sys
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import BufferedInputFile, CallbackQuery, Message
 
 from src.config import ALLOWED_USERS, BOT_TOKEN, DB_NAME
@@ -17,8 +19,11 @@ from src.services.keyboads import (
 from src.services.main import BotService
 
 dp = Dispatcher()
-
 bs = BotService()
+
+
+class DateState(StatesGroup):
+    waiting_for_date_input = State()
 
 
 @dp.message(Command("start", "menu"))
@@ -29,7 +34,7 @@ async def command_start_handler(message: Message) -> None:
     if message.from_user.id not in ALLOWED_USERS:
         await message.answer("You are not welcome here!")
     await message.answer(
-        bs.get_menu(
+        bs.get_menu_text(
             name=message.from_user.full_name,
             start=True if message.text == "/start" else False,
         ),
@@ -38,7 +43,7 @@ async def command_start_handler(message: Message) -> None:
 
 
 @dp.callback_query(F.data.startswith("menu_"))
-async def menu_callback(query: CallbackQuery) -> None:
+async def menu_callback(query: CallbackQuery, state: FSMContext) -> None:
     menu_item = query.data.split("_")[1]
     match menu_item:
         case "process":
@@ -50,9 +55,10 @@ async def menu_callback(query: CallbackQuery) -> None:
                 "Нажмите на кнопку для вывода статистики",
                 reply_markup=stats_keyboard_ikb.as_markup(),
             )
+            await state.set_state(DateState.waiting_for_date_input)
         case "last":
             await query.message.edit_text(
-                bs.get_statistics("last"), reply_markup=menu_back_ikb.as_markup()
+                bs.get_last_check(), reply_markup=menu_back_ikb.as_markup()
             )
         case "download":
             with open(f"db/{DB_NAME}.db", "rb") as db_from_buffer:
@@ -62,7 +68,7 @@ async def menu_callback(query: CallbackQuery) -> None:
                 )
         case "main":
             await query.message.edit_text(
-                bs.get_menu(start=False), reply_markup=menu_main_ikb.as_markup()
+                bs.get_menu_text(start=False), reply_markup=menu_main_ikb.as_markup()
             )
 
 
@@ -72,6 +78,15 @@ async def stats_callback(query: CallbackQuery):
     await query.message.edit_text(
         bs.get_statistics(period), reply_markup=stats_back_ikb.as_markup()
     )
+
+
+@dp.message(DateState.waiting_for_date_input)
+async def process_stats_input(message: Message, state: FSMContext):
+    date_to_parse = message.text
+    await message.answer(
+        f"Вы ввели: {date_to_parse}", reply_markup=stats_back_ikb.as_markup()
+    )
+    await state.clear()
 
 
 async def main() -> None:
